896a9446f9e30ed7ae7c834caec84873c8a97785
atiyeh <atiyeh.85@gmail.com>
2023-07-06 11:07:15 -0600

    Remove Element

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -6,8 +6,14 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        int[] num1 =new int[] { 8, 10, 14,0,0,0 };
-        int[] num2 = new int[]{ 2, 11, 15 };
-     var res= MergeSortedArray.Merge_Sorted_Array(num1, num1.Length-num2.Length ,num2,num2.Length);
+        //Merge_Sorted_Array
+        //   int[] num1 =new int[] { 8, 10, 14,0,0,0 };
+        //   int[] num2 = new int[]{ 2, 11, 15 };
+        //var res= MergeSortedArray.Merge_Sorted_Array(num1, num1.Length-num2.Length ,num2,num2.Length);
+
+
+        //RemoveElement
+        int[] nums = new int[]{3,3};
+        var res = RemoveElement.Remove_Element(nums,3);
     }
 }
---
+++ Problems/RemoveElement.cs
@@ -0,0 +1,49 @@
+﻿namespace Problems
+{
+    internal class RemoveElement
+    {
+        public static int Remove_Element(int[] nums, int val)
+        {
+            // Solution #1
+            int k = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    k++;
+                }
+            }
+            if (k != 0)
+            {
+                int j = nums.Length - 1;
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    if (nums[i] == val && j > i)
+                    {
+                        while (nums[j] == val && j > i)
+                        {
+                            j--;
+                        }
+                        nums[i] = nums[j];
+                        nums[j] = val;
+                    }
+                }
+            }
+            return k;
+
+
+
+
+            // Soluion #2
+            //int k = 0;
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //    if (nums[i] != val)
+            //    {
+            //        nums[k++] = nums[i];
+            //    }
+            //}
+            //return k;
+        }
+    }
+}
